ee9adf9448cf1511acbd3deea99401053dfc1dea
vuvantoann <toanvu1728@gamil.com>
2025-09-22 21:29:04 +0700

    Thêm authMiddleware cho route private

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -3,17 +3,17 @@ import { questionRoutes } from './question.route'
 import { userRoutes } from './user.route'
 import { topicRoutes } from './topic.route'
 import { answerRoutes } from './answer.route'
-
+import * as authMiddleware from '../middlewares/auth.middleware'
 const routesV1 = (app: Express) => {
   const version = '/api/v1'
 
-  app.use(version + '/questions', questionRoutes)
+  app.use(version + '/questions', authMiddleware.requireAuth, questionRoutes)
 
   app.use(version + '/users', userRoutes)
 
-  app.use(version + '/topics', topicRoutes)
+  app.use(version + '/topics', authMiddleware.requireAuth, topicRoutes)
 
-  app.use(version + '/answers', answerRoutes)
+  app.use(version + '/answers', authMiddleware.requireAuth, answerRoutes)
 }
 
 export default routesV1
